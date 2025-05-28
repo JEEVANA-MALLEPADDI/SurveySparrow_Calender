7d11a0fbca95522bbf686454d9032a0c4236d607
jeevana mallepaddi <127713904+JEEVANA-MALLEPADDI@users.noreply.github.com>
2025-05-28 12:38:45 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react-swc'
+// import tailwindcss from '@tailwindcss/vite'
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react(),tailwindcss(),],
+// })
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
-// https://vite.dev/config/
+
 export default defineConfig({
-  plugins: [react(),tailwindcss(),],
+  base: '/calender/',  // <-- Add this line for GitHub Pages
+  plugins: [react(), tailwindcss()],
 })
