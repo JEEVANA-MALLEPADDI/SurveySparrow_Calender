f8020203f4c24690170f4c4d48712e8596d1d46d
Jeevana <jeevanacse@gmail.com>
2025-05-29 14:28:56 +0530

    Fix vite base path for Vercel deployment

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: '/SurveySparrow_Calender/', 
+  
   plugins: [react(), tailwindcss()],
 })
